def190f648b51a31ec36932dd14310e9a524f4b3
rbuck <sutekistudent@gmail.com>
2010-11-05 16:01:48 -0600

    Adding documentation.

--- headers/Event.h
+++ headers/Event.h
@@ -13,20 +13,52 @@
 
 class IModule;
 
+/**
+ * Represents an Event which IModule objects will receive and react to.
+ */
 class Event : public Identifiable {
 public:
+
+    /**
+     * Constructs an Event object.
+     *
+     * @socket The socket to which to apply this Event to.
+     */
     Event(int socket);
+
+    /**
+     * Cleans up this Event object.
+     */
     virtual ~Event();
+
+    /**
+     * Will call a specific method on m depending on the type of this Event.
+     *
+     * @param m The module which to call a specific method on.
+     * @see IModule
+     */
     virtual void execute(IModule* m) = 0;
+
+    /**
+     * @return A reference to the socket.
+     */
     int & get_socket();
+
+    /**
+     * Default implemenation of less than.
+     *
+     * @param rhs The "right hand side" of the expression A < rhs, where A is this Event.
+     * @return True in each case.  Must be overridden in child classes and satisfy the above expression.
+     */
     virtual bool less_than(Event* rhs);
 private:
     int socket_;
-    
+
 };
 
 class EventComparator {
 public:
+
     bool operator()(Event*& t1, Event*& t2) {
         return t1->less_than(t2);
     }
--- headers/EventSet.h
+++ headers/EventSet.h
@@ -15,29 +15,55 @@
 
 using namespace std;
 
+/**
+ * Keeps a set of Events.  This class is dependent on the fact that an Event inherits from Identifiable.
+ * This class is thread safe.
+ */
 class EventSet {
 public:
 
+    /**
+     * Construct a new EventSet object.
+     */
     EventSet() {
         sem_.init(1);
     }
 
+    /**
+     * Cleans up this EventSet object.
+     */
     virtual ~EventSet() {
 
     }
 
+    /**
+     * Adds event to the set of internal Event objects.
+     *
+     * @param event The event to add to the set of internal Event objects.
+     */
     virtual void add(Event* event) {
         sem_.wait();
         ids_.insert(event->get_id());
         sem_.post();
     }
 
+    /**
+     * Removes event from the set of internal Event objects.
+     *
+     * @param event The event to remove from the set of internal Event objects.
+     */
     virtual void remove(Event* event) {
         sem_.wait();
         ids_.erase(event->get_id());
         sem_.post();
     }
 
+    /**
+     * Checks to see if event is in the set of internal Event objects.
+     *
+     * @param event The event to check.
+     * @return True if event is in the set, false otherwise.
+     */
     virtual bool contains(Event* event) {
         sem_.wait();
         bool val = ids_.find(event->get_id()) != ids_.end();
