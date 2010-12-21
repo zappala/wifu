522eaaee55f0c1ef0b342a354a7845f81d6b4750
Scott Erickson <theerickson@gmail.com>
2010-12-21 15:17:14 -0700

    NumberGenerator -> RandomNumberSet

---
+++ test/RandomNumberSetTest.h
@@ -0,0 +1,24 @@
+/*
+ * NumberGeneratorTest.h
+ *
+ *  Created on: Dec 20, 2010
+ *      Author: erickson
+ */
+
+#ifndef NUMBERGENERATORTEST_H_
+#define NUMBERGENERATORTEST_H_
+
+#include "UnitTest++.h"
+#include "RandomNumberSet.h"
+
+using namespace std;
+
+namespace {
+
+	SUITE(RandomNumberSet) {
+
+	}
+
+}
+
+#endif /* NUMBERGENERATORTEST_H_ */
