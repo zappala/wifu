61c88c2571516463126fac42586afce2b0a97de3
rbuck <sutekistudent@gmail.com>
2011-01-07 10:35:15 -0700

    Refactored Packet (and test)

--- test/end/SendPacketEventTest.h
+++ test/end/SendPacketEventTest.h
@@ -15,41 +15,33 @@
 using namespace std;
 
 namespace {
-	SUITE(SendPacketEvent) {
 
-		class IModuleDummyImplementation : public IModule {
-		public:
-			IModuleDummyImplementation() {
-				udp_sent = false;
-			}
+    SUITE(SendPacketEvent) {
 
-			void udp_send(Event* e) {
-				udp_sent = true;
-			}
+        class IModuleDummyImplementation : public IModule {
+        public:
 
-			bool udp_sent;
-		};
+            IModuleDummyImplementation() {
+                udp_sent = false;
+            }
 
-		TEST(udp_send) {
-//			IModuleDummyImplementation dummyImodule;
-//			CHECK(dummyImodule.udp_sent == false);
-//
-//
-//			int data_length = 10;
-//			unsigned char data[data_length];
-//			string source_address("127.0.0.1");
-//			string destination_address("127.0.0.1");
-//			struct wifu_end_header header;
-//			header.packet_length = data_length + sizeof(struct wifu_end_header);
-//			Packet packet(&header, data, 10, source_address, destination_address);
-//
-//
-//			SendPacketEvent sendPacketEvent(100, &packet);
-//			sendPacketEvent.execute(&dummyImodule);
-//
-//			CHECK(dummyImodule.udp_sent == true);
-		}
-	}
+            void udp_send(Event* e) {
+                udp_sent = true;
+            }
+
+            bool udp_sent;
+        };
+
+        TEST(udp_send) {
+            IModuleDummyImplementation dummyImodule;
+            CHECK(dummyImodule.udp_sent == false);
+            Packet* p;
+            SendPacketEvent sendPacketEvent(100, p);
+            sendPacketEvent.execute(&dummyImodule);
+
+            CHECK_EQUAL(true, dummyImodule.udp_sent);
+        }
+    }
 }
 
 #endif /* SENDPACKETEVENTTEST_H_ */
