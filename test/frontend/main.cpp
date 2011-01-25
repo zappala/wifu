03e6f6afefc263a8f30b52e3798c3c6c0ae11021
Scott Erickson <theerickson@gmail.com>
2011-01-25 10:13:55 -0700

    new test_frontend main

---
+++ test/frontend/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * main.cc
+ *
+ *  Created on: Dec 27, 2010
+ *      Author: erickson
+ */
+
+#include "gtest/gtest.h"
+
+//void change_dir() {
+//    int size = 1000;
+//    char buf[size];
+//    getcwd(buf, size);
+//    string path = buf;
+//    string bin = "bin";
+//
+//    if (path.find(bin) == string::npos) {
+//        chdir(bin.c_str());
+//    }
+//}
+
+int main(int argc, char** argv) {
+//    change_dir();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
