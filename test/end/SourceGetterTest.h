377f7e23e8310f0fad4951a3eae38d4046deb615
rbuck <sutekistudent@gmail.com>
2011-01-31 15:02:44 -0700

    made it so tests can be run on any ilab machine.

--- test/end/SourceGetterTest.h
+++ test/end/SourceGetterTest.h
@@ -20,7 +20,7 @@ namespace {
 
     SUITE(SourceGetter) {
 
-        // TODO: These tests can only be run on ilab1
+        // TODO: These tests can only be run on the ilab machine
         // We should make it generic
         // Also, we need to test it on the mesh nodes (or another machine with more than one interface)
 
@@ -31,13 +31,32 @@ namespace {
             CHECK_EQUAL(expected, result);
         }
 
+        string get_ip_address() {
+            string address;
+
+            int s = socket(AF_INET, SOCK_DGRAM, 0);
+            if (s <= 0) {
+                perror("SourceGetterTest::get_ip_address(): socket()");
+                exit(EXIT_FAILURE);
+            }
+
+            struct ifreq ifr;
+            strcpy(ifr.ifr_name, "eth0");
+
+            if (!ioctl(s, SIOCGIFADDR, &ifr)) {
+                address = inet_ntoa(((struct sockaddr_in *) & ifr.ifr_addr)->sin_addr);
+            }
+            close(s);
+            return address;
+        }
+
         TEST(AddressResolve) {
-            string expected = "192.168.21.101";
+            string expected = get_ip_address();
             for (int i = 1; i < 8; i++) {
                 char buffer[1000];
                 memset(buffer, 0, 1000);
                 sprintf(buffer, "192.168.21.10%d", i);
-                
+
                 string destination = buffer;
                 string result = sg.get_source_address(destination);
                 CHECK_EQUAL(expected, result);
@@ -47,7 +66,7 @@ namespace {
         TEST(AddressResolveCache) {
             // No way to ensure we hit the cache...
             string destination = "192.168.21.102";
-            string expected = "192.168.21.101";
+            string expected = get_ip_address();
             string result = sg.get_source_address(destination);
             CHECK_EQUAL(expected, result);
         }
