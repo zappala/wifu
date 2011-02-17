9c2f509293e018158f66defcb77fadbba10adf4b
Scott Erickson <theerickson@gmail.com>
2011-02-17 14:38:25 -0700

    Message separated and modified to correct a compile error (why wasn't this caught when it was in the header? ..) even more reason to separate the files

--- headers/Message.h
+++ headers/Message.h
@@ -9,45 +9,26 @@
 #define	_MESSAGE_H
 
 #include <string>
+#include <cstring>
 
 using namespace std;
 
-class Message{
+class Message {
 public:
-    Message(int size, char * message) {
-        strcpy(c_string_, message);
-    }
-
-    Message(int size, string & message) {
-        string_ = message;
-        c_string_ = 0;
-    }
+    Message(int size, char*);
+    Message(int size, string&);
 
     // Copy constructor
     // Assignment operator
 
-    ~Message();
-
-    string to_s() {
-        if(c_string_) {
-            return string(c_string_);
-        }
-        return string_;
-    }
-
-    char * c_str() {
-        if(c_string_) {
-            return string(c_string_);
-        }
-        return string_.c_str();
-    }
-    
+    virtual ~Message();
+    string to_s();
+    char* c_str();
 
 private:
     int size_;
     string string_;
-    char * c_string_;
-    
+    char* c_string_;
 };
 
 #endif	/* _MESSAGE_H */
