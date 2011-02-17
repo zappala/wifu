f0a0b4a976f1be0bb15850d6c3de01748885b33f
Scott Erickson <theerickson@gmail.com>
2011-02-17 14:11:50 -0700

    AddressPort separated

--- headers/AddressPort.h
+++ headers/AddressPort.h
@@ -35,9 +35,7 @@ public:
      * @param address Address to store
      * @param port Port to store
      */
-    AddressPort(string& address, uint16_t port) : address_(address), port_(port) {
-        init(address.c_str(), port);
-    }
+    AddressPort(string& address, uint16_t port);
 
     /**
      * Constructor: Stores address and port.
@@ -45,28 +43,14 @@ public:
      * @param address Address to store
      * @param port Port to store
      */
-    AddressPort(const char* address, uint16_t port) : address_(string(address)), port_(port) {
-        init(address, port);
-    }
+    AddressPort(const char* address, uint16_t port);
 
     /**
      * Constructor: Stores address and port inside obj.
      *
      * @param obj sockaddr_in pointer which contains an address and a port.
      */
-    AddressPort(struct sockaddr_in* obj) {
-
-        assert(obj);
-
-        char ip_address[INET_ADDRSTRLEN];
-        u_int32_t saddr = obj->sin_addr.s_addr;
-        inet_ntop(AF_INET, &saddr, ip_address, INET_ADDRSTRLEN);
-
-        address_ = string(ip_address);
-        port_ = ntohs(obj->sin_port);
-
-        memcpy(&data_, obj, sizeof (struct sockaddr_in));
-    }
+    AddressPort(struct sockaddr_in* obj);
 
     /**
      * Copy Constructor
@@ -74,9 +58,7 @@ public:
      *
      * @param original The AddressPort from which to copy data out of
      */
-    AddressPort(const AddressPort& original) : address_(original.address_), port_(original.port_) {
-        memcpy(&data_, &original.data_, sizeof (struct sockaddr_in));
-    }
+    AddressPort(const AddressPort& original);
 
     /**
      * Assignment Operator
@@ -84,14 +66,7 @@ public:
      *
      * @param original The AddressPort from which to copy data out of
      */
-    AddressPort & operator=(const AddressPort& original) {
-        if (this != &original) {
-            address_ = original.address_;
-            port_ = original.port_;
-            memcpy(&data_, &original.data_, sizeof (struct sockaddr_in));
-        }
-        return *this;
-    }
+    AddressPort & operator=(const AddressPort& original);
 
     /**
      * Operator equals
@@ -99,9 +74,7 @@ public:
      * @param other The "other" Address port to compare this one with
      * @return True if the addresses and the ports are equivialent, false otherwise
      */
-    bool operator==(const AddressPort& other) const {
-        return address_ == other.address_ && port_ == other.port_;
-    }
+    bool operator==(const AddressPort& other) const;
 
     /**
      * Operator not equals
@@ -109,56 +82,39 @@ public:
      * @param other The "other" Address port to compare this one with
      * @return True if either the addresses or ports do not match, false if addresses and ports are equivalent
      */
-    bool operator!=(const AddressPort& other) const {
-        return !operator ==(other);
-    }
+    bool operator!=(const AddressPort& other) const;
 
     /**
      * Destructor
      */
-    virtual ~AddressPort() {
-
-    }
+    virtual ~AddressPort();
 
     /**
      * @return A reference to the address
      */
-    string& get_address() {
-        return address_;
-    }
+    string& get_address();
 
     /**
      * @return A reference to the port
      */
-    uint16_t get_port() {
-        return port_;
-    }
+    uint16_t get_port();
 
     /**
      * @return Reference to the data in struct format
      */
-    struct sockaddr_in& get_network_struct() {
-        return data_;
-    }
+    struct sockaddr_in& get_network_struct();
 
     /**
      * @return Pointer to the data in struct format
      */
-    struct sockaddr_in* get_network_struct_ptr() {
-        return &data_;
-    }
+    struct sockaddr_in* get_network_struct_ptr();
 
     /**
      * Returns a string representation of this AddressPort
      *
      * @return A string representation of this AddressPort
      */
-    string to_s() {
-        stringstream s;
-        s << "Address: " << get_address() << " ";
-        s << "Port: " << get_port();
-        return s.str();
-    }
+    string to_s();
 
 private:
     /**
@@ -182,13 +138,7 @@ private:
      * @param address Address to store
      * @param port Port to store
      */
-    void init(const char* address, uint16_t port) {
-        data_.sin_family = AF_INET;
-        data_.sin_port = htons(port);
-
-        if (!inet_aton(address, &data_.sin_addr))
-            throw InvalidAddressException();
-    }
+    void init(const char* address, uint16_t port);
 };
 
 /**
@@ -205,9 +155,7 @@ public:
      * @param ap2 the RHS of the expression ap1 < ap2
      * @return True if t1 < t2, false otherwise.
      */
-    bool operator()(AddressPort* const& ap1, AddressPort* const& ap2) {
-        return ap1->to_s() < ap2->to_s();
-    }
+    bool operator()(AddressPort* const& ap1, AddressPort* const& ap2);
 };
 
 #endif	/* _ADDRESSPORT_H */
