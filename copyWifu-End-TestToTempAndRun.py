940a7553509484f8a34856ad2d2a21882461d3f2
Scott Erickson <theerickson@gmail.com>
2011-02-24 15:05:39 -0700

    script refactor

--- copyWifu-End-TestToTempAndRun.py
+++ copyWifu-End-TestToTempAndRun.py
@@ -6,7 +6,10 @@ if __name__ == '__main__':
 	os.system("cp bin/wifu-end-test /tmp")
 	os.chdir("/tmp")
 	os.system("rm coverage.csexe")
+	
+	runCommand = "./wifu-end-test"
 	if len(sys.argv) > 1:
-		os.system(" ".join(["./wifu-end-test", " ".join(sys.argv[1:])]))
+		runCommand = " ".join([runCommand, " ".join(sys.argv[1:])])
+		os.system(runCommand)
 	else:
-		os.system("./wifu-end-test")
+		os.system(runCommand)
