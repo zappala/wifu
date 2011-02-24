f7dea8facfeed3e1a130378fd10208e84fb5bf14
Scott Erickson <theerickson@gmail.com>
2011-02-24 15:07:52 -0700

    Python script runners now take args that will be passed to the test binary. (cherry-picking from commit 6cf0608fd24efdc49e66121d8f44cd0ba3bf0d49) script refactor (cherry-picking from commit 940a7553509484f8a34856ad2d2a21882461d3f2)

--- copyWifu-End-TestToTempAndRun.py
+++ copyWifu-End-TestToTempAndRun.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 
 if __name__ == '__main__':
 	os.system("cp bin/wifu-end-test /tmp")
 	os.chdir("/tmp")
 	os.system("rm coverage.csexe")
-	os.system("./wifu-end-test")
+	
+	runCommand = "./wifu-end-test"
+	if len(sys.argv) > 1:
+		runCommand = " ".join([runCommand, " ".join(sys.argv[1:])])
+		os.system(runCommand)
+	else:
+		os.system(runCommand)
