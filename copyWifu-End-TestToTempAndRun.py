6cf0608fd24efdc49e66121d8f44cd0ba3bf0d49
Scott Erickson <theerickson@gmail.com>
2011-02-24 14:06:21 -0700

    Python script runners now take args that will be passed to the test binary.

--- copyWifu-End-TestToTempAndRun.py
+++ copyWifu-End-TestToTempAndRun.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 
 if __name__ == '__main__':
 	os.system("cp bin/wifu-end-test /tmp")
 	os.chdir("/tmp")
 	os.system("rm coverage.csexe")
-	os.system("./wifu-end-test")
+	if len(sys.argv) > 1:
+		os.system(" ".join(["./wifu-end-test", " ".join(sys.argv[1:])]))
+	else:
+		os.system("./wifu-end-test")
--- copyWifu-Frontend-TestToTempAndRun.py
+++ copyWifu-Frontend-TestToTempAndRun.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 
 if __name__ == '__main__':
 	os.system("cp bin/wifu-frontend-test /tmp")
 	os.system("cp bin/wifu-end /tmp")
 	os.chdir("/tmp")
 	os.system("rm coverage.csexe")
-	os.system("./wifu-frontend-test")
+	if len(sys.argv) > 1:
+		os.system(" ".join(["./wifu-frontend-test", " ".join(sys.argv[1:])]))
+	else:
+		os.system("./wifu-frontend-test")
