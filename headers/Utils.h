4641ccd114da573c94e7c9df0b65fee2ca281588
Scott Erickson <theerickson@gmail.com>
2011-03-01 13:44:26 -0700

    get_timespec_future_time documentation added to header

--- headers/Utils.h
+++ headers/Utils.h
@@ -19,6 +19,14 @@ using namespace std;
 
 class Utils {
 public:
+	/**
+	 * Sets the timespec struct passed in to -seconds- and -nanoseconds- into
+	 * the future.
+	 *
+	 * @param seconds the # of seconds to set the timespec forward
+	 * @param nanoseconds the # of nanoseconds to set the timespec forward
+	 * @param ts pointer to the timespec to set into the future
+	 */
     static void get_timespec_future_time(int seconds, long int nanoseconds, struct timespec* ts);
 
     static string itoa(int i);
