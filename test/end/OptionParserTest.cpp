18e17a00f778009e94ace01e0d7d769e739c856f
Scott Erickson <theerickson@gmail.com>
2011-03-12 13:49:08 -0700

    code that shows OptionParser doesn't work?

--- test/end/OptionParserTest.cpp
+++ test/end/OptionParserTest.cpp
@@ -10,10 +10,28 @@
 
 #include "gtest/gtest.h"
 #include "OptionParser.h"
+#include <string>
 
 using namespace std;
 
+#define parser OptionParser::instance()
+
 namespace {
+
+	static struct option long_options[] = {
+		{"network", required_argument, NULL, 0},
+		{"mockfile", required_argument, NULL, 0},
+		{0, 0, 0, 0}
+	};
+
+	TEST(OptionParser, all) {
+		string networkOption = "--network=standard";
+		char* const args[3] = {"./awesome", const_cast<char*>(networkOption.c_str()), NULL};
+
+		parser.parse(2, args, long_options);
+
+		ASSERT_EQ(true, parser.present(networkOption));
+	}
 
 }
 
