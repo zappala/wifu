39294584fdcee8b35d04bd22eea8c8835367014d
rbuck <sutekistudent@gmail.com>
2011-06-01 10:59:34 -0600

    edited rich's file

--- src/states/tcp-ap/SendRateLimiter.cc
+++ src/states/tcp-ap/SendRateLimiter.cc
@@ -16,36 +16,30 @@ SendRateLimiter::~SendRateLimiter() {
 }
 
 //rate of 1 packet per (seconds + nanoseconds)
-void SendRateLimiter::setRate(int seconds,  long int nanoseconds)
-{
+
+void SendRateLimiter::setRate(int seconds, long int nanoseconds) {
     sendSeconds_ = seconds;
     sendNanos_ = nanoseconds;
 }
 
-int SendRateLimiter::getSeconds()
-{
+int SendRateLimiter::getSeconds() {
     return sendSeconds_;
 }
 
-long int SendRateLimiter::getNanoseconds()
-{
+long int SendRateLimiter::getNanoseconds() {
     return sendNanos_;
 }
 
-void SendRateLimiter::state_send_packet(Context* c, SendPacketEvent* e)
-{
+void SendRateLimiter::state_send_packet(Context* c, SendPacketEvent* e) {
     //cout << "SendRateLimiter::send_packet: Entered\n";
-    if(canSend_)
-    {
+    if (canSend_) {
         //cout << "SendRateLimiter::send_packet: Sending packet: " << e->get_packet() << "\n";
         canSend_ = 0;
         //dispatch NetworkSendPacketEvent, etc.
         NetworkSendPacketEvent* netSendEvent = new NetworkSendPacketEvent(e->get_socket(), e->get_packet());
         Dispatcher::instance().enqueue(netSendEvent);
         setTimer(e->get_socket());
-    }
-    else
-    {
+    } else {
         //cout << "SendRateLimiter::send_packet: Enqueuing packet: " << e->get_packet() << "\n";
         //enqueue to send later
         queue_->enqueue(e->get_packet());
@@ -53,19 +47,16 @@ void SendRateLimiter::state_send_packet(Context* c, SendPacketEvent* e)
     }
 }
 
-void SendRateLimiter::state_timer_fired(Context* c, TimerFiredEvent* e)
-{
+void SendRateLimiter::state_timer_fired(Context* c, TimerFiredEvent* e) {
     //cout << "SendRateLimiter::timer_fired: got timer: " << e->get_timeout_event() << endl;
     //make sure this is ours
-    if(waitingTimeout_ == 0 || e->get_timeout_event() != waitingTimeout_)
-    {
+    if (waitingTimeout_ == 0 || e->get_timeout_event() != waitingTimeout_) {
         //cout << "SendRateLimiter::timer_fired: got timer: " << e->get_timeout_event() << " but wanted " << waitingTimeout_ << endl;
         return;
     }
 
     //if data to send, dispatch the NetworkSendPacketEvent
-    if(!queue_->isEmpty())
-    {
+    if (!queue_->is_empty()) {
         //cout << "SendRateLimiter::timer_fired: sending queued data: " << endl;
         //send packet
         WiFuPacket* packet = queue_->dequeue();
@@ -73,16 +64,13 @@ void SendRateLimiter::state_timer_fired(Context* c, TimerFiredEvent* e)
         NetworkSendPacketEvent* netSendEvent = new NetworkSendPacketEvent(e->get_socket(), packet);
         Dispatcher::instance().enqueue(netSendEvent);
         setTimer(e->get_socket());
-    }
-    else
-    {
+    } else {
         canSend_ = 1;
-        waitingTimeout_= 0;
+        waitingTimeout_ = 0;
     }
 }
 
-void SendRateLimiter::setTimer(Socket* s)
-{
+void SendRateLimiter::setTimer(Socket* s) {
     TimeoutEvent* timeout_event = new TimeoutEvent(s, sendSeconds_, sendNanos_);
     waitingTimeout_ = timeout_event;
     Dispatcher::instance().enqueue(timeout_event);
