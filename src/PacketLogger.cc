3c132bca2261bf0fd9ae26f8e7bb38f5cb415a2f
rbuck <sutekistudent@gmail.com>
2011-06-27 10:10:31 -0600

    packet logger now logging packets fake ethernet header in place need to fix it up a bit: issues with ip protocol field, checksums, efficient logging, etc.

--- src/PacketLogger.cc
+++ src/PacketLogger.cc
@@ -16,6 +16,7 @@ PacketLogger::~PacketLogger() {
     close_log();
 }
 
+#include "packet/TCPPacket.h"
 void PacketLogger::log(WiFuPacket* packet) {
     lock_.wait();
 
@@ -24,12 +25,20 @@ void PacketLogger::log(WiFuPacket* packet) {
     if (fileout_.fail())
         throw IOError();
 
+    const char fake_ethernet_header[14] = {0x00, 0x50, 0x56, 0xb7, 0x16, 0xc7, 0x00, 0x0f, 0xfe, 0xfe, 0x52, 0x17, 0x08, 0x00};
+
     packet_header_.ts_sec = time_.tv_sec;
     packet_header_.ts_usec = time_.tv_usec;
-    packet_header_.incl_len = packet->get_ip_tot_length();
-    packet_header_.orig_len = packet->get_ip_tot_length();
+    packet_header_.incl_len = packet->get_ip_tot_length() + sizeof (fake_ethernet_header);
+    packet_header_.orig_len = packet->get_ip_tot_length() + sizeof (fake_ethernet_header);
+
+    TCPPacket* p = (TCPPacket*) packet;
+    p->set_ip_protocol(6);
+    p->calculate_and_set_ip_checksum();
+    p->calculate_and_set_tcp_checksum();
 
     fileout_.write(reinterpret_cast<const char*> (&packet_header_), sizeof (PcapPacketHeader));
+    fileout_.write(fake_ethernet_header, sizeof (fake_ethernet_header));
     fileout_.write(reinterpret_cast<const char*> (packet->get_payload()), packet->get_ip_tot_length());
 
     close_log();
@@ -65,7 +74,7 @@ void PacketLogger::fill_in_file_header() {
     file_header_.version_minor = 4;
     file_header_.thiszone = 0;
     file_header_.sigfigs = 0;
-    file_header_.snaplen = MTU;
+    file_header_.snaplen = USHRT_MAX;
     file_header_.network = 1; //Ethernet
 }
 
