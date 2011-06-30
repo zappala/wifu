d532810becbeb09fdabb9421e9634d244f115268
rbuck <sutekistudent@gmail.com>
2011-06-30 11:48:44 -0600

    made protected methods virtual

--- headers/states/TCPTahoeReliabilityState.h
+++ headers/states/TCPTahoeReliabilityState.h
@@ -36,24 +36,24 @@ public:
     virtual void state_receive(Context* c, QueueProcessor<Event*>* q, ReceiveEvent* e);
 
 protected:
-    void start_timer(Context* c, Socket* s);
-    void reset_timer(Context* c, Socket* s);
-    void cancel_timer(Context* c, Socket* s);
-    void update_rto(Context* c, TCPTimestampOption* ts);
-    void resend_data(Context* c, QueueProcessor<Event*>* q, Socket* s, ResendReason reason);
-    void create_and_dispatch_received_data(Context* c, QueueProcessor<Event*>* q, ReceiveEvent* e);    
-    void create_and_dispatch_ack(QueueProcessor<Event*>* q, Socket* s);
+    virtual void start_timer(Context* c, Socket* s);
+    virtual void reset_timer(Context* c, Socket* s);
+    virtual void cancel_timer(Context* c, Socket* s);
+    virtual void update_rto(Context* c, TCPTimestampOption* ts);
+    virtual void resend_data(Context* c, QueueProcessor<Event*>* q, Socket* s, ResendReason reason);
+    virtual void create_and_dispatch_received_data(Context* c, QueueProcessor<Event*>* q, ReceiveEvent* e);
+    virtual void create_and_dispatch_ack(QueueProcessor<Event*>* q, Socket* s);
 
     // <editor-fold defaultstate="collapsed" desc="state_send_packet() helper functions">
-    void set_sequence_number_and_update_window_variables(Context* c, TCPPacket* p);
-    void set_ack_number_and_bit(Context* c, TCPPacket* p);
-    void check_and_start_timer(Context*c, SendPacketEvent* e);
-    void append_control_bytes_to_send_buffer(SendPacketEvent* e);
-    void insert_timestamp(Context* c, TCPPacket* p);
+    virtual void set_sequence_number_and_update_window_variables(Context* c, TCPPacket* p);
+    virtual void set_ack_number_and_bit(Context* c, TCPPacket* p);
+    virtual void check_and_start_timer(Context*c, SendPacketEvent* e);
+    virtual void append_control_bytes_to_send_buffer(SendPacketEvent* e);
+    virtual void insert_timestamp(Context* c, TCPPacket* p);
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="state_receive_packet() helper functions">
-    void update_echo_reply(Context* c, TCPPacket* p);
+    virtual void update_echo_reply(Context* c, TCPPacket* p);
 
     /**
      * Handles the ack portion of the incoming packet
@@ -62,12 +62,12 @@ protected:
      * @param e The Event received from the network interface
      * @return True if we may continue (and try to process data), false if it is an invalid ack
      */
-    bool handle_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
-    void handle_valid_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
-    void handle_duplicate_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
-    void handle_control_bits_and_data(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
-    void handle_control_bits(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
-    void handle_data(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual bool handle_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual void handle_valid_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual void handle_duplicate_ack(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual void handle_control_bits_and_data(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual void handle_control_bits(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
+    virtual void handle_data(Context* c, QueueProcessor<Event*>* q, NetworkReceivePacketEvent* e);
     // </editor-fold>
     
 };
