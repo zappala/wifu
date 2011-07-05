920edd70a5ab20ad8e76174a0851a53d1c4ed65b
rbuck <sutekistudent@gmail.com>
2011-07-05 14:51:33 -0600

    commented the SocketEvent

--- headers/events/framework_events/SocketEvent.h
+++ headers/events/framework_events/SocketEvent.h
@@ -11,12 +11,41 @@
 #include "LibraryEvent.h"
 #include "Socket.h"
 
+using namespace std;
+
+/**
+ * Represents a socket() call by the application.
+ *
+ * @see Event
+ * @see FrameworkEvent
+ * @see LibraryEvent
+ *
+ */
 class SocketEvent : public LibraryEvent {
 public:
+
+    /**
+     * Constructs a SocketEvent.
+     *
+     * @param message Message received from the front end library containing the information about the socket() BSD socket API call and some bookkeeping information.
+     * @param file The file associated with a Unix socket to write any response to the application.
+     * @param s The Socket object to which this Event belongs.
+     */
     SocketEvent(string& message, string& file, Socket* socket);
 
+    /**
+     * Destructor.
+     */
     virtual ~SocketEvent();
 
+    /**
+     * Calls IModule::imodule_library_socket() and passes this SocketEvent in as the argument.
+     *
+     * @param m The module which to call IModule::imodule_library_socket() on.
+     * @see Event::execute()
+     * @see IModule
+     * @see IModule::imodule_library_socket()
+     */
     void execute(IModule* m);
 
 };
--- src/events/framework_events/SocketEvent.cc
+++ src/events/framework_events/SocketEvent.cc
@@ -1,9 +1,13 @@
 #include "events/framework_events/SocketEvent.h"
 
-SocketEvent::SocketEvent(string& message, string& file, Socket* socket) : LibraryEvent(message, file, socket) {}
+SocketEvent::SocketEvent(string& message, string& file, Socket* socket) : LibraryEvent(message, file, socket) {
 
-SocketEvent::~SocketEvent() {}
+}
+
+SocketEvent::~SocketEvent() {
+
+}
 
 void SocketEvent::execute(IModule* m) {
-	m->imodule_library_socket(this);
+    m->imodule_library_socket(this);
 }
