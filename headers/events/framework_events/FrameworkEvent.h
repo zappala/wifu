4ebb6ff66f1d0f71862c0abe79b5ed07483a0e5a
rbuck <sutekistudent@gmail.com>
2011-07-05 11:48:29 -0600

    framework and protocol events documented

--- headers/events/framework_events/FrameworkEvent.h
+++ headers/events/framework_events/FrameworkEvent.h
@@ -11,9 +11,30 @@
 #include "events/Event.h"
 #include "defines.h"
 
+/**
+ * Represents an Event with a LOW priority.
+ * Certain Protocols may need to ensure that they process all ProtocolEvent's before processing another FrameworkEvent.
+ * Events such as an application request and receiving packets are of lower priority than receiving information about a buffer (being empty or full).
+ * 
+ * @see Event
+ * @see ProtocolEvent
+ * @see Priority
+ */
 class FrameworkEvent : public Event {
 public:
+
+    /**
+     * Constructs an Event with a LOW Priority.
+     * @param s The socket to which to apply this Event to.
+     *
+     * @see Event
+     * @see Priority
+     */
     FrameworkEvent(Socket* s);
+
+    /**
+     * Destructor.
+     */
     virtual ~FrameworkEvent();
 };
 
--- headers/events/protocol_events/ProtocolEvent.h
+++ headers/events/protocol_events/ProtocolEvent.h
@@ -11,9 +11,30 @@
 #include "events/Event.h"
 #include "defines.h"
 
+/**
+ * Represents an Event with a HIGH priority.
+ * Certain Protocols may need to ensure that they process all ProtocolEvents before processing another FrameworkEvent.
+ * Events such as an application request and receiving packets are of lower priority than receiving information about a buffer (being empty or full).
+ *
+ * @see Event
+ * @see ProtocolEvent
+ * @see Priority
+ */
 class ProtocolEvent : public Event {
 public:
+
+    /**
+     * Constructs a HIGH Priority Event.
+     * @param s The socket to which to apply this Event to.
+     *
+     * @see Event
+     * @see Priority
+     */
     ProtocolEvent(Socket* s);
+
+    /**
+     * Destructor.
+     */
     virtual ~ProtocolEvent();
 };
 
