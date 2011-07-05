e3f9bb582a9ccf1a777e11fa2fd23a4c6d871176
rbuck <sutekistudent@gmail.com>
2011-07-05 12:29:02 -0600

    documented the AcceptEvent

--- headers/events/framework_events/AcceptEvent.h
+++ headers/events/framework_events/AcceptEvent.h
@@ -10,12 +10,38 @@
 
 #include "LibraryEvent.h"
 
+/**
+ * Represents an accept() call by the application.
+ *
+ * @see Event
+ * @see FrameworkEvent
+ * @see LibraryEvent
+ * 
+ */
 class AcceptEvent : public LibraryEvent {
 public:
+
+    /**
+     * Constructs an AcceptEvent.
+     * 
+     * @param message Message received from the front end library containing the information about the accept() BSD socket API call and some bookkeeping information.
+     * @param file The file to write any response to the application.  Represents a Unix socket.
+     * @param s The Socket object to which this Event belongs.
+     */
     AcceptEvent(string& message, string& file, Socket* s);
 
+    /**
+     * Destructor.
+     */
     virtual ~AcceptEvent();
 
+    /**
+     * Calls IModule::imodule_library_accept() and passes this AcceptEvent in as the argument.
+     * 
+     * @param m The module which to call IModule::imodule_library_accept() on.
+     * @see Event
+     * @see IModule
+     */
     void execute(IModule* m);
 
 };
