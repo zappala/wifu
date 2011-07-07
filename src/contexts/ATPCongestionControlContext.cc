60e76cdec0805263c3fb79562138e8dc31387776
Philip Lundrigan <philiplundrigan@gmail.com>
2011-07-07 12:27:29 -0600

    Can keep track of time between receive buffer events and the buffer size Added some comments

--- src/contexts/ATPCongestionControlContext.cc
+++ src/contexts/ATPCongestionControlContext.cc
@@ -7,7 +7,7 @@
 
 #include "../../headers/contexts/ATPCongestionControlContext.h"
 
-ATPCongestionControlContext::ATPCongestionControlContext(u_int32_t iss) : TCPTahoeCongestionControlContext(iss), average_delay_(0) {
+ATPCongestionControlContext::ATPCongestionControlContext(u_int32_t iss) : TCPTahoeCongestionControlContext(iss), average_delay_(0), receive_buffer_size_(0) {
 	set_state(new Waiting());
 }
 
@@ -40,13 +40,24 @@ u_int32_t ATPCongestionControlContext::get_reading_delay(){
 	return read_delay_;
 }
 
-u_int32_t ATPCongestionControlContext::get_time_difference(){
-	struct timespec stop = start_;
+void ATPCongestionControlContext::start_timer(){
 	clock_gettime(CLOCK_REALTIME, &start_);
+}
+
+u_int32_t ATPCongestionControlContext::stop_timer(){
+	struct timespec stop;
+	clock_gettime(CLOCK_REALTIME, &stop);
 
 	// converting difference to microseconds
-	u_int32_t diff = (start_.tv_sec - stop.tv_sec) * 1000000.0 + ((start_.tv_nsec - stop.tv_nsec) / 1000.0);
+	u_int32_t diff = (stop.tv_sec - start_.tv_sec) * 1000000.0 + ((stop.tv_nsec - start_.tv_nsec) / 1000.0);
 
 	return diff;
 }
 
+int ATPCongestionControlContext::get_receive_buffer_size(){
+	return receive_buffer_size_;
+}
+
+void ATPCongestionControlContext::set_receive_buffer_size(int size){
+	receive_buffer_size_ = size;
+}
