a94811e594d60f9f3a29bd1d64ea932b765d5c4f
rbuck <sutekistudent@gmail.com>
2011-12-30 14:48:48 -0700

    Updated bash script

--- preliminary/multiple_configurations.sh
+++ preliminary/multiple_configurations.sh
@@ -18,104 +18,104 @@
 #python ./preliminary.py -u rbuck -c config/ilab_wired_1000_10KB.conf
 
 # Mesh wireless 1-3 hops, 1 thread
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops.conf
 
 # Mesh wireless 1-3 hops, 2 threads
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_2_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_2_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_2_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_2_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_2_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_2_threads.conf
 
 # Mesh wireless 1-3 hops, 5 threads
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_5_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_5_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_5_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_5_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_5_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_5_threads.conf
 
 # Mesh wireless 1-3 hops, 10 threads
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_10_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-one-hop.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-one-hop.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_1_hop_10_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_10_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-two-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-two-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_2_hops_10_threads.conf
 
-cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
-./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
-cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
-python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_10_threads.conf
+#cd /home/rbuck/NetBeansProjects/mesh-config/custom_config
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/other-three-hops.json
+#./config.sh -k -r -m "Randy's thesis work" ./conf/randy-three-hops.json
+#cd /home/rbuck/NetBeansProjects/wifu-end/preliminary
+#python ./preliminary.py -u rbuck -c config/mesh_wireless_3_hops_10_threads.conf
 
 # ilab scalability
 python ./preliminary.py -u rbuck -c config/ilab_wired_10_1MB_10_threads.conf
