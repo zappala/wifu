a53df414272d8b424d564cc9fcd6e7f0d829c286
Randy Buck <sutekistudent@gmail.com>
2011-12-30 14:53:31 -0700

    added new tests

--- preliminary/multiple_configurations.sh
+++ preliminary/multiple_configurations.sh
@@ -129,3 +129,12 @@ python ./preliminary.py -u rbuck -c config/ilab_wired_1000_1MB_10_threads.conf
 #python ./preliminary.py -u rbuck -c config/ilab_wired_10_1MB_100_threads.conf
 #python ./preliminary.py -u rbuck -c config/ilab_wired_100_1MB_100_threads.conf
 #python ./preliminary.py -u rbuck -c config/ilab_wired_1000_1MB_100_threads.conf
+
+# ilab big files
+python ./preliminary.py -u rbuck -c config/ilab_wired_10_100MB.conf
+python ./preliminary.py -u rbuck -c config/ilab_wired_100_100MB.conf
+python ./preliminary.py -u rbuck -c config/ilab_wired_1000_100MB.conf
+
+python ./preliminary.py -u rbuck -c config/ilab_wired_10_500MB.conf
+python ./preliminary.py -u rbuck -c config/ilab_wired_100_500MB.conf
+python ./preliminary.py -u rbuck -c config/ilab_wired_1000_500MB.conf
